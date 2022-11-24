de20b748fa20116051005739f391d8b9c16a33ed
Robiul Islam Sagar <robiul100.me@gmail.com>
2022-11-24 07:41:34 +0600

    Slider added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,15 +1,18 @@
 import { ArrowLeft, ArrowRight } from "@mui/icons-material"
+import { useState } from "react"
 import styled from "styled-components"
+import { sliderItems } from "../data"
 
 const Container = styled.div`
     height: 100vh;
     width: 100%;
     position: relative;
+    overflow: hidden;
 `
 
 const ArrowContainer = styled.div`
-    height: 35px;
-    width: 35px;
+    height: 45px;
+    width: 45px;
     border-radius: 50%;
     background: #cfcfcf;
     display: flex;
@@ -19,6 +22,7 @@ const ArrowContainer = styled.div`
     transition: .2s;
     cursor: pointer;
     position: absolute;
+    z-index: 5;
     top: 50%;
     left: ${props => props.dir === 'left' && '10px'};
     right: ${props => props.dir === 'right' && '10px'};
@@ -30,52 +34,123 @@ const ArrowContainer = styled.div`
 
 const Wrapper = styled.div`
     height: 100vh;
+    width: 100%;
+    display: flex;
+    transform: translateX(-${props => props.sliderIndex * 100}vw);
+    transition: .4s;
 `
 
 const Slide = styled.div`
-
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    gap: 40px;
+    flex: 1;
+    width: 100vw;
 `
 
 const ImgContainer = styled.div`
-
+    width: 50vw;
+    flex: 1;
+    border: 1px solid red;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
-const Img = styled.div`
-
+const Img = styled.img`
+    width: 100%;
+    height: 90%;
+    object-fit: cover;
 `
 
 const TextContainer = styled.div`
-
+    flex: 1;
+    width: 50vw;
 `
 
-const Title = styled.div`
-
+const Title = styled.h1`
+    font-size: 53px;
+    font-weight: 600;
+    text-transform: uppercase;
 `
 
-const Desc = styled.div``
-const Btn = styled.div``
+const Desc = styled.div`
+    font-size: 18px;
+    color: #555;
+    letter-spacing: 2px;
+    margin: 40px 0
+`
+
+const Btn = styled.div`
+    padding: 10px;
+    background: transparent;
+    border: 1px solid #555;
+    font-size: 19px;
+    text-transform: uppercase;
+    font-weight: 500;
+    display: inline-block;
+    border-radius: 5px;
+    position: relative;
+    cursor: pointer;
+    transition: .2s;
+    z-index: 1;
+    
+    &::before{
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left:0;
+        width: 100%;
+        height: 0%;
+        background: #490;
+        z-index: -1;
+        transition: .2s;
+    }
+
+    &:hover{
+        color: #fff;
+    }
+
+    &:hover::before{
+        height: 100%;
+    }
+`
 
 const Slider = () => {
+    const [sliderIndex, setSliderIndex] = useState(0)
+    const sliderLength = sliderItems.length
+
+    const handleSlide = direction => {
+        direction === "left" ? sliderIndex > 0 ? setSliderIndex(sliderIndex - 1) : setSliderIndex(sliderLength - 1) :
+            sliderIndex < sliderLength - 1 ? setSliderIndex(sliderIndex + 1) : setSliderIndex(0)
+        console.log(sliderIndex, " = ", sliderIndex * 100, " vw");
+    }
+
+
     return (
         <Container>
-            <ArrowContainer dir="left">
+            <ArrowContainer dir="left" onClick={() => handleSlide("left")}>
                 <ArrowLeft sx={{ fontSize: '35px' }} />
             </ArrowContainer>
 
-            <Wrapper>
-                <Slide>
-                    <ImgContainer>
-                        <Img></Img>
-                    </ImgContainer>
-                    <TextContainer>
-                        <Title>Autumn collection 1</Title>
-                        <Desc>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi fugiat maxime rem repellat nulla maiores doloribus culpa nihil consectetur tenetur.</Desc>
-                        <Btn>shop now</Btn>
-                    </TextContainer>
-                </Slide>
+            <Wrapper sliderIndex={sliderIndex}>
+                {sliderItems.map(slide => (
+                    <Slide key={slide.id}>
+                        <ImgContainer>
+                            <Img src={slide.img} />
+                        </ImgContainer>
+                        <TextContainer>
+                            <Title>{slide.title} </Title>
+                            <Desc>{slide.desc} </Desc>
+                            <Btn>shop now</Btn>
+                        </TextContainer>
+                    </Slide>
+                ))}
+
             </Wrapper>
 
-            <ArrowContainer dir="right">
+            <ArrowContainer dir="right" onClick={() => handleSlide("right")}>
                 <ArrowRight sx={{ fontSize: '35px' }} />
             </ArrowContainer>
         </Container>
